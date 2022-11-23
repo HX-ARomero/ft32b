b94e6972eb4850ae00b4189ae2ed382ed9f21a6b
HX-ARomero <almafuerte.ariel.1978@gmail.com>
2022-11-23 16:53:13 -0300

    react-redux

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,11 +1,40 @@
 import styles from "./Card.module.css";
 import { Link } from "react-router-dom";
+import { addFavorites, deleteFavorites } from "../redux/actions";
+import { connect } from "react-redux";
+import { useEffect, useState } from "react";
 
-export default function Card(props) {
-   console.log(props);
+export function Card(props) {
+   //console.log(props);
+   const [isFav, setIsFav] = useState(false)
+
+   useEffect(() => {
+      props.myFavorites.forEach((fav) => {
+         if (fav.id === props.detailId) {
+            setIsFav(true);
+         }
+      });
+   }, [props.myFavorites]);
+
+   function handleFavorite() {
+      if(isFav) {
+         setIsFav(false)
+         props.deleteFavorites(props.detailId)
+      } else {
+         setIsFav(true)
+         props.addFavorites(props.detailId)
+      }
+   }
+
    return (
       <div className={styles.container}>
          <div className={styles.buttonContainer}>
+            { isFav ? (
+               <button onClick={handleFavorite}>❤️</button>
+                  ) : (
+               <button onClick={handleFavorite}>🤍</button>
+                  )
+            }
             <button onClick={props.onClose}>X</button>
             </div>
             <Link to={`/detail/${props.detailId}`}>
@@ -19,3 +48,22 @@ export default function Card(props) {
       </div>
    );
 }
+
+export function mapDispatchToProps(dispatch) {
+   return {
+      addFavorites: function(id) {
+         dispatch(addFavorites(id))
+      },
+      deleteFavorites: function(id) {
+         dispatch(deleteFavorites(id))
+      }
+   }
+}
+
+export function mapStateToProps(state) {
+   return {
+      myFavorites: state.myFavorites
+   }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Card)
